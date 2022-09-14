7ea4c0196f17305f3c2969db71b423abe06233ce
kiwidestroyer <kiwidestroyer@pokerplatform.io>
2022-09-14 16:22:19 +0800

    Added rate service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RateController } from './rate/rate.controller';
+import { RateService } from './rate/rate.service';
 
 @Module({
   imports: [],
   controllers: [AppController, RateController],
-  providers: [AppService],
+  providers: [AppService, RateService],
 })
 export class AppModule {}
--- src/rate/rate.controller.ts
+++ src/rate/rate.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { RateService } from 'src/rate/rate.service';
 
 @Controller('rate')
-export class RateController {}
+export class RateController {
+  constructor(private rateService: RateService) {}
+}
